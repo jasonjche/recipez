bac86847371bbd0ff853f8429dc8dc88871c44fd
Jason He <jasonhe.md@gmail.com>
2023-04-19 22:18:24 -0400

    First commit

---
+++ src/lib/components/GroceryList.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let items: string[];
+</script>
+
+<div class="bg-white rounded-lg shadow-lg p-6 mt-8">
+    <h2 class="text-2xl font-bold mb-4">Grocery List</h2>
+    <ul class="list-inside list-disc text-lg">
+        {#each items as item}
+            <li>{item}</li>
+        {/each}
+    </ul>
+</div>
